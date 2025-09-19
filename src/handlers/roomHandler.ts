2eb8fda9bccc24c347adc4a0ebfc75f2993f89bd
Himansh <himanshbansal200521@gmail.com>
2025-09-19 11:33:31 +0530

    ADD: New user room joining logic

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,16 +1,36 @@
 import { Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
+import type IRoomParams from "../interfaces/IRoomParams.js";
+
+const rooms : Record<string, string[]> = {};
 
 const roomHandler = (socket: Socket) => {
+
     const createRoom = () => {
         const roomId = uuidv4();
         socket.join(roomId);
+
+        rooms[roomId] = []; //create a new entry for the room
         socket.emit("room-created", {roomId});
         console.log("New room created with ID:", roomId);
     };
 
-    const joinRoom = ({ roomId }: { roomId: string }) => {
-        console.log("New user has joined the room", roomId);
+    // the below function is executed everytime a user joins
+    const joinRoom = ({ roomId, peerId }: IRoomParams ) => {
+        
+        if (rooms[roomId]) {
+            //If the given roomId exists in the memory db
+            console.log("New user has joined the room", roomId, "with peerId:", peerId);
+            // the moment  new user joins, add the peerId to the key of roomId
+            rooms[roomId].push(peerId);
+            socket.join(roomId);
+
+            // below event is for logging purpose only
+            socket.emit("get-users", {
+                roomId,
+                participants: rooms[roomId]
+            })
+        };
     };
 
     // when we will call the above two functions
